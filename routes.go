ffe7b6d67180f55c0a6d1a7cede983f26b9c8c93
Matt Parker <mdparker89@gmail.com>
2014-04-16 11:13:08 -0400

    first commit

---
+++ routes.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"github.com/ant0ine/go-urlrouter"
+)
+
+var router urlrouter.Router = urlrouter.Router{
+	Routes: []urlrouter.Route{
+		urlrouter.Route{
+			PathExp: "/template/invoice",
+			Dest: map[string]func(map[string]interface{}, []byte) ([]byte, error, int){
+				"GET": invoice,
+			},
+		},
+		urlrouter.Route{
+			PathExp: "/pdf/invoice",
+			Dest: map[string]func(map[string]interface{}, []byte) ([]byte, error, int){
+				"GET": invoice,
+			},
+		},
+		urlrouter.Route{
+			PathExp: "/template/agreement",
+			Dest: map[string]func(map[string]interface{}, []byte) ([]byte, error, int){
+				"GET": agreement,
+			},
+		},
+		urlrouter.Route{
+			PathExp: "/pdf/agreement",
+			Dest: map[string]func(map[string]interface{}, []byte) ([]byte, error, int){
+				"GET": agreement,
+			},
+		},
+	},
+}
