397fd38c3c59673b5d7b405d59186889ea1c3290
Matt Parker <mdparker89@gmail.com>
2014-05-14 19:35:01 -0400

    fix agreement payment number logic

--- handlers.go
+++ handlers.go
@@ -29,8 +29,8 @@ func agreement(params map[string]interface{}, body []byte) ([]byte, error, int) 
 	var paymentTotal float64
 
 	for _, payment := range payments {
-		if payments[0].IsDeposit {
-			payment.Number -= payment.Number
+		if !payments[0].IsDeposit {
+			payment.Number += 1
 		}
 		paymentTotal += payment.AmountDue
 	}
@@ -115,12 +115,13 @@ func invoice(params map[string]interface{}, body []byte) ([]byte, error, int) {
 	var data struct {
 		Agreement  *Agreement             `json:"agreement"`
 		Payment    *Payment               `json:"payment"`
+		Payments   []*Payment             `json:"payments"`
 		Tasks      []*Task                `json:"tasks"`
 		Freelancer map[string]interface{} `json:"freelancer"`
 		Client     map[string]interface{} `json:"client"`
 	}
-	// json.Unmarshal([]byte(`{"agreement":{"acceptsBankTransfer":true,"acceptsCreditCard":true,"agreementID":"ed356d73-70b7-485c-7cac-c1e42b49ca86","archived":false,"clientID":"096d76ac-ac5c-489c-60ca-d0bde5aac605","freelancerID":"5275dc12-2429-42ad-536d-ccfdba2fc91c","lastAction":{"date":"2014-04-11T14:21:46.867388667-04:00","name":"accepted","userID":"5275dc12-2429-42ad-536d-ccfdba2fc91c"},"lastModified":"2014-04-11T14:21:46.867389586-04:00","lastSubAction":null,"proposedServices":"<p>Create a blog</p>\n","title":"Sample Agreement","version":1,"versionID":"ed356d73-70b7-485c-7cac-c1e42b49ca86"}, "payment":{"amountDue":2000,"amountPaid":0,"number":1,"dateExpected":"2014-04-18T04:00:00Z","id":"c1ac7d37-91ee-46ee-6493-c6c00774a47b","isDeposit":false,"lastAction":null,"paymentItems":[{"amountDue":1000,"hours":10,"rate":100,"subtaskID":"345","taskID":"123","title":"subtask"}, {"amountDue":1000,"hours":0,"rate":0,"subtaskID":"","taskID":"","title":"subtask2"}],"title":"Halfway Payment","versionID":"ed356d73-70b7-485c-7cac-c1e42b49ca86"},"tasks":[{ "sample":true, "dateExpected": "2014-04-11T04:00:00Z", "hours": 0, "id": "123", "isPaid": false, "lastAction": { "name": "completed"}, "subTasks": [ { "dateExpected": "0001-01-01T00:00:00Z", "hours": 0, "id": "7da4396b-69b8-4505-56b3-b22aa34cf158", "isPaid": false, "lastAction": { "name": "completed"}, "subTasks": null, "title": "Ideation", "versionID": ""}, { "dateExpected": "0001-01-01T00:00:00Z", "hours": 0, "id": "e40c49a3-f4b1-4ca3-470a-8b80819bcdbc", "isPaid": false, "lastAction": { "name": "completed"}, "subTasks": null, "title": "Wireframes", "versionID": ""}, { "dateExpected": "0001-01-01T00:00:00Z", "hours": 0, "id": "6ceb4dd8-d586-4136-605d-a4e2b85ccb13", "isPaid": false, "lastAction": { "name": "completed"}, "subTasks": null, "title": "Review", "versionID": "" }], "title": "Design", "versionID": "ed356d73-70b7-485c-7cac-c1e42b49ca86"}, { "sample":true, "dateExpected": "2014-04-18T04:00:00Z", "hours": 0, "id": "98f79d60-8fe1-4f35-4f55-f6d0bf8caa47", "isPaid": false, "lastAction": null, "subTasks": [ { "dateExpected": "0001-01-01T00:00:00Z", "hours": 0, "id": "11a0bd22-4dd0-4ade-6dd4-54ba5ff49f99", "isPaid": false, "lastAction": null, "subTasks": null, "title": "Prototype in HTML/CSS", "versionID": ""}, { "dateExpected": "0001-01-01T00:00:00Z", "hours": 0, "id": "fe421c0d-80a9-4487-6158-cf35528ebb17", "isPaid": false, "lastAction": null, "subTasks": null, "title": "Review", "versionID": "" }], "title": "Mockup", "versionID": "ed356d73-70b7-485c-7cac-c1e42b49ca86"}, { "sample":true, "dateExpected": "2014-04-18T04:00:00Z", "hours": 0, "id": "0b47680f-40ca-483b-7803-4e5258bac5bf", "isPaid": false, "lastAction": null, "subTasks": [ { "dateExpected": "0001-01-01T00:00:00Z", "hours": 0, "id": "46786360-8c22-4f0d-7570-5e6042c5ae9f", "isPaid": false, "lastAction": null, "subTasks": null, "title": "Finish coding blog", "versionID": ""}, { "dateExpected": "0001-01-01T00:00:00Z", "hours": 0, "id": "18960288-3cc8-4855-4fb1-758da517c95c", "isPaid": false, "lastAction": null, "subTasks": null, "title": "Move to production", "versionID": ""}, { "dateExpected": "0001-01-01T00:00:00Z", "hours": 0, "id": "e5ebc27b-8a13-441a-7869-28e237f6ee62", "isPaid": false, "lastAction": null, "subTasks": null, "title": "Review", "versionID": "" }], "title": "Final Product", "versionID": "ed356d73-70b7-485c-7cac-c1e42b49ca86" }], "freelancer":{"isRegistered":true, "firstName":"Matt", "lastName":"Parker", "email":"mdparker89@gmail"},"client":{"isRegistered":true, "firstName":"Marcus", "lastName":"Ellison", "email":"marcus@gmail"}}`), &data)
 	json.Unmarshal(body, &data)
+	data.Payment.Number += 1
 	agreement := data.Agreement
 	tasks := data.Tasks
 	freelancer := data.Freelancer
